a96f05435a9a42de7afe9fe5c1faf5266ff83e8f
Mark <markmaw@gmail.com>
2015-03-27 00:35:46 -0400

    fixed

--- mpi_bug2.c
+++ mpi_bug2.c
@@ -12,8 +12,7 @@
 
 int main (int argc, char *argv[])
 {
-int numtasks, rank, tag=1, alpha, i;
-float beta;
+int numtasks, rank, tag=1, alpha, i, beta;
 MPI_Request reqs[10];
 MPI_Status stats[10];
 
@@ -34,11 +33,12 @@ if (rank == 0) {
 
 if (rank == 1) {
   for (i=0; i<10; i++) {
-    MPI_Irecv(&beta, 1, MPI_FLOAT, 0, tag, MPI_COMM_WORLD, &reqs[i]);
+    MPI_Irecv(&beta, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &reqs[i]);
     MPI_Wait(&reqs[i], &stats[i]);
-    printf("Task %d received = %f\n",rank,beta);
+    printf("Task %d received = %d\n",rank,beta);
     }
   }
 
 MPI_Finalize();
+return 0;
 }
