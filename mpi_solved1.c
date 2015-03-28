be9acae66af81ee17c5b8bd76f4ca81566aab622
Mark <markmaw@gmail.com>
2015-03-28 17:49:23 -0400

    added comment

--- mpi_solved1.c
+++ mpi_solved1.c
@@ -25,6 +25,11 @@ if (rank == 0) {
     printf("Numtasks=%d. Only 2 needed. Ignoring extra...\n",numtasks);
   dest = rank + 1;
   source = dest;
+  /*
+    SOLUTION: fixed *tag* to be the same value for each process 
+    so messages sent with a tag will be recieved by a process looking
+    for a message with the same tag. 
+  */
   tag = numtasks;
   rc = MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
   printf("Sent to task %d...\n",dest);
@@ -35,6 +40,9 @@ if (rank == 0) {
 else if (rank == 1) {
   dest = rank - 1;
   source = dest;
+  /*
+    SOLUTION: here as well
+  */  
   tag = numtasks;
   rc = MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
   printf("Received from task %d...\n",source);
